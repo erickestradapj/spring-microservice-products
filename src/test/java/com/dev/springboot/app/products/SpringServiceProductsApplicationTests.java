ac44cd59249ccc9c4488ce79893f7c49bb16db80
Erick Estrada <erickestradapj@gmail.com>
2021-11-04 15:18:25 -0500

    starting project

---
+++ src/test/java/com/dev/springboot/app/products/SpringServiceProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.springboot.app.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
